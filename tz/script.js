d0b61cd581c9084459ce90fa358eb4464e843e49
roman.m <hookatama@gmail.com>
2017-10-03 14:02:00 +0300

    edits

--- tz/script.js
+++ tz/script.js
@@ -1,4 +1,4 @@
-var newArr;
+var arr;
 
 $(document).ready(function(){
     
@@ -6,32 +6,7 @@ $('#startButton').click(function(){
       
       $('#startResult').empty();
 
-      var selectValue = +$('#numbs').val();   
-    
-      var arr = [];
-      
-      for(var i = 0; i < selectValue; i++){
-        var rand = Math.floor(Math.random() * (selectValue+1));
-        arr.push(rand);
-      }
-      
-      newArr = arr;
-
-      newArr.reduce(function(result, item, i){
-        if ( i < 100) {
-            $('#startResult').append(item + ' ');
-            if ( (i+1)%10 == 0 ) {
-                $('#startResult').append( $('<br/>') );
-            }
-        }
-      }, 0);
-  });
-    
-$('#sortButton').click(function(){
-    
-      $('#sortResult').empty();
-    
-      var selectValue = +$('#numbs').val();   
+      var selectValue = +$('#numbs').val();
     
       var fromValue = +$('#from').val();
       if (fromValue == ''){
@@ -41,25 +16,32 @@ $('#sortButton').click(function(){
       if (toValue == ''){
               toValue = 10};
     
-    
-      if ((fromValue < 0) || (isNaN(fromValue)) || (fromValue > toValue) || (toValue > selectValue) || (isNaN(toValue))){
-          $('#sortResult').text('Wrong arguments! Please, try again')
-      }
-      else{
-    
-      var arr = [];
-    
-          for(var i = 0; i < newArr.length; i++){
-              if ((newArr[i] >= fromValue) && (newArr[i] <= toValue)){
-                  arr.push(newArr[i]);
-              }
+      arr = [];
+
+          for(var i = 0; i < selectValue; i++){
+            var rand = Math.floor(Math.random() * (toValue - fromValue + 1) + fromValue);
+            arr.push(rand);
           }
+
+          arr.reduce(function(result, item, i){
+            if ( i < 100) {
+                $('#startResult').append(item + ' ');
+                if ( (i+1)%10 == 0 ) {
+                    $('#startResult').append( $('<br/>') );
+                }
+            }
+          }, 0);
+  });
+    
+$('#sortButton').click(function(){
+    
+      $('#sortResult').empty(); 
           
           arr.sort(function(a, b) {
             if (a > b) return 1;
             if (a < b) return -1;
-            if (a == b) return 0;
-            
+            if (a === b) return 0;
+
           });
           
           arr.reduce(function(result, item, i){
@@ -70,7 +52,6 @@ $('#sortButton').click(function(){
                 } 
             }
           }, 0);
-      }
 });
     
 });
